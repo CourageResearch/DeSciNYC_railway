f5a4f1fbcd46ccb2bb375496fbb68a57d6550183
Courage Research <91306375+CourageResearch@users.noreply.github.com>
2026-07-16 14:33:27 -0400

    Mark Peptides talk as past event

---
+++ db/migrations/0015_mark_descinyc49_done.sql
@@ -0,0 +1,6 @@
+UPDATE events
+SET active = false,
+    yt_uuid = 'HSf_6ZIhjA0',
+    slides = 'https://docs.google.com/presentation/d/1rMmGkGr9JmCkb4eV8EyfhjcZClRsQZhR4jHd_r9Gyas/edit?usp=sharing'
+WHERE luma_id = 'evt-NyxpH2NdNO4DotF'
+   OR luma_url IN ('https://luma.com/descinyc49', 'https://lu.ma/descinyc49');
